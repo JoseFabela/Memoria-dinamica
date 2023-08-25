b81cdf6724f6cc79a20963d9d626d25cfa142cc6
JoseFabela <jfabela522@gmail.com>
2023-08-25 00:33:30 -0600

    Agregar archivos de proyecto.

---
+++ Memoria dinamica/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memoria_dinamica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> dynamicList = new List<int>();
+
+            Console.WriteLine("Enter numbers (0 to stop):");
+
+            while (true)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (input == 0)
+                    break;
+
+                dynamicList.Add(input);
+            }
+
+            Console.WriteLine("Entered numbers:");
+            foreach (int number in dynamicList)
+            {
+                Console.Write(number + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+                 
